2b70fcb3f8e22e2ae093b0dedfb8c1767d1dd8e9
dainv <dainv@miagi-so.net>
2025-02-25 14:58:40 +0700

    sửa file name index.js

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Image from '~/components/Image';
+import { Link } from 'react-router-dom';
+
+const cx = classNames.bind(styles);
+
+function AccountItem({ data }) {
+    return (
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image
+                className={cx('avatar')}
+                src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                alt="ddx"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>{data.first_name}</span>
+                    {data.tick && (
+                        <FontAwesomeIcon
+                            className={cx('check')}
+                            icon={faCheckCircle}
+                        />
+                    )}
+                </h4>
+                <span className={cx('username')}>{data.nickname}</span>
+            </div>
+        </Link>
+    );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object,
+};
+export default AccountItem;
